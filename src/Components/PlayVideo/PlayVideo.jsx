8861e9991c2fc104d972c96b1bc74f7e2364b8f6
Garrett Uichanco <garrettuichanco@gmail.com>
2026-09-01 10:29:30 -0700

    feat: fix 404 not found error when typing in specific url and added fullscreen video functionality.

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -48,7 +48,12 @@ const PlayVideo = () => {
 
     return (
         <div className='play-video'>
-            <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen"
+                referrerpolicy="strict-origin-when-cross-origin"
+                allowfullscreen>
+            </iframe>
             <h3>{apiData ? apiData.snippet.title : "Title Here"}</h3>
             <div className="play-video-info">
                 <p>{apiData ? value_converter(apiData.statistics.viewCount) : "16K"} Views &bull; {apiData ? moment(apiData.snippet.publishedAt).fromNow() : ""}</p>
